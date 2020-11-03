8e7ab9db050acbf72b4a6fe7debbe859f72d5fa9
dannyhannyford <dannyhannyford@gmail.com>
2020-11-03 13:16:59 -0800

    webpack and react setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style.css';
+
+const App = () => (
+  <div>
+    <p>hello from react</p>
+  </div>
+);
+
+export default App;
--- server/start.js
+++ server/start.js
@@ -4,11 +4,8 @@ const path = require('path');
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 const app = express();
 
-// app.use(express.static(PUBLIC_DIR));
+app.use(express.static(PUBLIC_DIR));
 
-app.get('/', (req, res) => {
-  res.status(200).send('hello');
-});
 // TODO connection to database
 
 // app.get('/api/pastepoints/:user_id/:karma', (req, res) => {
