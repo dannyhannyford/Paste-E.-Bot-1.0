62274789fd2139ce84f74a19f6ab4f14be392343
dannyhannyford <dannyhannyford@gmail.com>
2020-11-04 15:28:43 -0800

    displays the top 10 users with the most karma from the db

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../style.css';
 
-const App = () => (
-  <div>
-    <p>hello from react</p>
-  </div>
-);
+const App = () => {
+  const [pasteData, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('api/users')
+      .then(({ data }) => setData(data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div>
+      {pasteData.map((user, idx) => (
+          <p key={user + idx}>{`${idx + 1} ${user.username} | ${user.karma}`}</p>
+      ))}
+    </div>
+  );
+};
 
 export default App;
